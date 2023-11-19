199cba98863cc0d7f9b83f6c1159f6a085b8c208
FelipePrieto22 <felipeprieto2321@gmail.com>
2023-11-19 00:56:56 -0300

    a

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -103,7 +103,7 @@ const Calendar = () => {
         </button>
         <button
           className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-          onClick={handleNextWeek}
+          onClick={handleNextWeek} 
         >
           Semana Siguiente
         </button>
